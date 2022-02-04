140cd1e089e4c5baf8b28638981c352d865ed779
Chris Foster <cdbfoster@gmail.com>
2022-02-04 17:55:04 -0500

    Remove everything from the poker prelude, into the crate root.

--- poker/src/lib.rs
+++ poker/src/lib.rs
@@ -5,11 +5,9 @@ mod rank;
 mod state;
 mod suit;
 
-pub mod prelude {
-    pub use crate::card::Card;
-    pub use crate::deck::{full_deck, Deck};
-    pub use crate::hand::{Hand, HandComparator};
-    pub use crate::rank::Rank;
-    pub use crate::state::{Player, State, Value};
-    pub use crate::suit::Suit;
-}
+pub use self::card::Card;
+pub use self::deck::{full_deck, CardMap, Deck};
+pub use self::hand::{Hand, HandComparator};
+pub use self::rank::Rank;
+pub use self::state::{Player, State, Value};
+pub use self::suit::Suit;
