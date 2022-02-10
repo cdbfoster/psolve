43e927b4fe87eb3aa9e49387dfa6e873f0df79b6
Chris Foster <cdbfoster@gmail.com>
2022-02-10 10:35:21 -0500

    Gave the nodes a stable field order and an iterator for their children.

--- game-tree/src/node.rs
+++ game-tree/src/node.rs
@@ -7,15 +7,60 @@ pub struct RootNode {
     pub first_child: Volatile<NodePtr>,
 }
 
-pub struct ActionNode<A, P> {
-    pub action: A,
-    pub parameters: *mut P,
-    pub next_sibling: NodePtr,
-    pub first_child: Volatile<NodePtr>,
+impl RootNode {
+    /// Caller must ensure that the iterator returned does not outlive this node.
+    pub fn children(&self) -> NodePtrIterator {
+        NodePtrIterator::new(self.first_child.read())
+    }
 }
 
-pub struct ChanceNode<C> {
-    pub result: C,
-    pub next_sibling: NodePtr,
+#[repr(C)]
+pub struct ActionNode<A, P> {
+    pub next_sibling: *mut ActionNode<A, P>, // Must be first.
     pub first_child: Volatile<NodePtr>,
+    pub parameters: *mut P,
+    pub action: A,
+}
+
+impl<A, P> ActionNode<A, P> {
+    /// Caller must ensure that the iterator returned does not outlive this node.
+    pub fn children(&self) -> NodePtrIterator {
+        NodePtrIterator::new(self.first_child.read())
+    }
+}
+
+#[repr(C)]
+pub struct ChanceNode<C> {
+    pub next_sibling: *mut ChanceNode<C>, // Must be first.
+    pub first_child: Volatile<NodePtr>,
+    pub result: C,
+}
+
+impl<C> ChanceNode<C> {
+    /// Caller must ensure that the iterator returned does not outlive this node.
+    pub fn children(&self) -> NodePtrIterator {
+        NodePtrIterator::new(self.first_child.read())
+    }
+}
+
+pub struct NodePtrIterator {
+    next_node: NodePtr,
+}
+
+impl NodePtrIterator {
+    pub fn new(node: NodePtr) -> Self {
+        Self { next_node: node }
+    }
+}
+
+impl Iterator for NodePtrIterator {
+    type Item = NodePtr;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        (!self.next_node.is_null()).then(|| {
+            let ptr = self.next_node;
+            self.next_node = unsafe { *(ptr as *mut NodePtr) };
+            ptr
+        })
+    }
 }
